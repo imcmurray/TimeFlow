605cb9bd768bda3063d9ffafbe0bf672aa10a673
Ian McMurray <ian_mcmurray@utb.uscourts.gov>
2026-01-11 23:18:02 -0700

    Add Android platform support for Google Play Store submission.

---
+++ android/app/src/main/kotlin/com/example/timeflow/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.timeflow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/rinserepeatlabs/timeflow/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rinserepeatlabs.timeflow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()

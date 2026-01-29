15ce9da685376fc54a0100e77859cade0656fa31
imcmurray <imcmurray@users.noreply.github.com>
2026-01-29 06:13:20 +0000

    deploy: 5241ea2555369f658b5d89b5c33b8349c5173693

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"db373eb85a06eb3d706af362a45d42d0972f4b
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1844776621"
+    serviceWorkerVersion: "1732800084"
   }
 });
